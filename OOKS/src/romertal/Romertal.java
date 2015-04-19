cb92c4cce207569c117f0ae3189917f63b761182
Looft <anderslooft@gmail.com>
2015-04-19 16:11:25 +0200

    ITilV implementeret

--- OOKS/src/romertal/Romertal.java
+++ OOKS/src/romertal/Romertal.java
@@ -1,9 +1,26 @@
 package romertal;
 
 public class Romertal {
+	private int antalI;
 
 	public String calculate(String a, String b) {
-		return "MMCLXXVIII";
+		String s = new String();
+		s = a+b;
+		return s;
+	}
+
+	public String ITilV(String a, String b) {
+		String s = a+b;
+		for(int i = 0; i<s.length(); i++){
+			if(s.charAt(i) == 'I'){
+				antalI++;				
+			}
+		}
+		if(antalI >= 5){
+			s = "V";
+		}
+			
+		return s;
 	}
 
 }
---
+++ OOKS/test/test/ITest.java
@@ -0,0 +1,25 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import romertal.Romertal;
+
+public class ITest {
+
+	//@Test
+	public void test() {
+		Romertal r = new Romertal();
+		
+		assertEquals("V", r.calculate("IIII", "I"));
+	}
+	
+	@Test
+	public void testITilV() {
+		Romertal r = new Romertal();
+		
+		assertEquals("V", r.ITilV("IIII", "I"));
+	}
+
+}
